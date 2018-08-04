630f871244c65493790764a0a0b88617dc438d68
chandan <flychand@gmail.com>
2018-08-04 15:09:01 +0530

    Add project page

--- server.js
+++ server.js
@@ -2,25 +2,24 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port=process.env.PORT || 3000;
-
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
-// app.use((req, res, next) => {
-//   var now = new Date().toString();
-//   var log = `${now}: ${req.method} ${req.url}`;
-
-//   console.log(log);
-//   fs.appendFile('server.log', log + '\n');
-//   next();
-// });
-
 app.use((req, res, next) => {
-  res.render('maintenance.hbs');
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  //fs.appendFile('server.log', log + '\n');
+  next();
 });
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -42,6 +41,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
